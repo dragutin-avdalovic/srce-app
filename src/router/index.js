abeb3371b20c394e313ce5a9d8b2eed1b4f9550d
dragutin <dragutin.avdalovic@walter.ba>
2018-03-23 15:46:27 +0100

    Main of the app moved to root, web location is now http://localhost:8080/#/

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
 import About from '@/components/About'
 import Main from '@/components/Main'
 
@@ -10,18 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'Main',
+      component: Main
     },
     {
       path: '/about',
       name: 'About',
       component: About
-    },
-    {
-      path: '/main',
-      name: 'Main',
-      component: Main
     }
   ]
 })
