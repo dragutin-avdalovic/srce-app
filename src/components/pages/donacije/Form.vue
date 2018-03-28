2964162bf5537e851f181645322d6e09fca1ceef
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-03-28 14:25:00 +0200

    Form donations hidding unrequired fields

--- src/components/pages/donacije/Form.vue
+++ src/components/pages/donacije/Form.vue
@@ -13,17 +13,17 @@
           <span v-show="errors.has('type')" class="help-block">{{ errors.first('type') }}</span>
         </p>
       </div>
-      <div class="col-xl-12 col-lg-12 col-md-12 col-xs-12 col-12 form-group min-row-height" v-bind:class="{'has-error':errors.has('company')}">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-xs-12 col-12 form-group min-row-height" v-show="formData.type!=2" v-bind:class="{'has-error':errors.has('company')}">
         <label class="control-label" for="company">Naziv kompanije*</label>
         <p :class="{ 'control': true }">
-          <input v-validate="'required'" :class="{'input': true, 'has-error': errors.has('company') }" id="company" name="company" type="text" v-model="formData.company" class="form-control" placeholder="">
+          <input v-validate="''" :class="{'input': true, 'has-error': errors.has('company') }" id="company" name="company" type="text" v-model="formData.company" class="form-control" placeholder="">
           <span v-show="errors.has('company')" class="help-block">{{ errors.first('company') }}</span>
         </p>
       </div>
-      <div class="col-12 col-xl-6 col-md-6 col-xs-6 col-lg-6 form-group min-row-height" v-bind:class="{'has-error':errors.has('name')}">
+      <div class="col-12 col-xl-6 col-md-6 col-xs-6 col-lg-6 form-group min-row-height" v-show="formData.type===2" v-bind:class="{'has-error':errors.has('name')}">
         <label class="control-label" for="name">Ime i prezime*</label>
         <p :class="{ 'control': true }">
-          <input v-validate="'required'" :class="{'input': true, 'has-error': errors.has('name') }" name="name" type="text" v-model="formData.name" class="form-control" id="name" placeholder="">
+          <input v-validate="''" :class="{'input': true, 'has-error': errors.has('name') }" name="name" type="text" v-model="formData.name" class="form-control" id="name" placeholder="">
           <span v-show="errors.has('name')" class="help-block">{{ errors.first('name') }}</span>
         </p>
       </div>
@@ -94,6 +94,11 @@ export default{
   methods: {
     save: function () {
       console.log(this.formData)
+      if (this.formData.company === '') {
+        this.formData.company = null
+      } else if (this.formData.name === '') {
+        this.formData.name = null
+      }
       this.$emit('onDataEmit', this.formData)
     },
     closeModal: function () {
